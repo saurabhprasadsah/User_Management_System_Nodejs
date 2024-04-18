9bcf71036550cdf4befc45f5c35b7f75bc4e6fe4
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-04-18 11:06:40 +0530

    update

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from "express"
 import cors from "cors"
-
+import connectDB from './config/connectdb.js';
 
 const app = express()
 
-//cors policy will be handle frontend integration part.
-app.use(cors())
+
+const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT
 
 
 
-app.get("/name",(req, res) =>{
-    res.send("Hello saurabh")
-})
+
+//cors policy will be handle frontend integration part.
+app.use(cors())
 
 
+
+//JSON
+app.use(express.json())
+
+//Database connection
+connectDB(DATABASE_URL)
 
 app.listen(PORT, ()=>{
     console.log(`Server listening at http://localhost:${PORT}`);
